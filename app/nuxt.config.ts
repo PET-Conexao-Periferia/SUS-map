3db13b25cbaac28fd2d8cf3abaf940aa74fa77af
Guilherme Valença <guilherm3vrp@gmail.com>
2025-01-31 10:42:26 -0300

    feat(APP): add tailwind and scss

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -3,5 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   srcDir: 'src',
-  css: ['~/assets/css/main.css'],
+  css: [
+      '~/assets/css/main.scss',
+      '~/assets/css/variables.scss',
+      '~/assets/css/tailwind.scss',
+  ],
 });
