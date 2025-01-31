91a61aa1f4ea0dd7fdd5d6c7b468dd63b0c93717
hannasabrynna <hannasabrynna135@gmail.com>
2025-01-31 09:13:03 -0300

    feat(api): add migrations

---
+++ api/database/migrations/2025_01_31_115407_create_descriptions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('descriptions', function (Blueprint $table) {
+            $table->id();
+            $table->string( 'name', 255);
+            $table->string( 'contact', 255);
+            $table->unsignedBigInteger('id_location');
+            $table->unsignedBigInteger('id_openingTimes');
+            $table->timestamps();
+
+            $table->foreign('id_location')->references('id')->on('locations')->onDelete('cascade');
+            $table->foreign('id_openingTimes')->references('id')->on('opening_times')->onDelete('cascade');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('descriptions');
+    }
+};
