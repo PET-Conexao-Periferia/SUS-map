a047f4ad80db4bab7d8a3e0498e15e0c3a53da8a
hannasabrynna <hannasabrynna135@gmail.com>
2025-10-05 13:14:37 -0300

    feat: Controllers e rotas de Address, Service e Description

---
+++ api/routes/service.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\ServiceController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('services')
+    ->controller(ServiceController::class)
+    ->group(function () {
+
+        Route::middleware(['auth:sanctum', 'ability:user-admin'])
+            ->group(function () {
+
+                Route::post('location/{location}', 'store');
+
+                Route::put('{service}', 'update');
+                Route::delete('{service}', 'destroy');
+            });
+    });
